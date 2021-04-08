68b379d9cbb920efeb4f03a21ef938f18960b6f8
Fernando Cagua <efcaguab@gmail.com>
2021-04-08 19:12:17 +1200

    Remove ona as a host

--- R/retrieve-survey-data.R
+++ R/retrieve-survey-data.R
@@ -2,14 +2,13 @@
 #'
 #' Specifies the Host URL of the API to use
 #'
-#' @param api Either "kobo", "kobohr", "ona", or a custom (full) URL. API URLs
-#'   are made available for KoBo Toolbox ("kobo",
+#' @param api Either "kobo", "kobohr", or a custom (full) URL. API URLs are made
+#'   available for KoBo Toolbox ("kobo",
 #'   \url{https://kc.kobotoolbox.org/api/v1/}), KoBo Humanitarian Response
-#'   ("kobohr", \url{https://kc.humanitarianresponse.info/api/v1/}), Ona ("ona",
-#'   \url{https://ona.io/api/v1/}) and Unhcr ("unhcr",
-#'   \url{https://kobocat.unhcr.org/api/v1/}) . For your own installation, or
-#'   other installations using the same API but accessed at a different URL,
-#'   enter the full URL.
+#'   ("kobohr", \url{https://kc.humanitarianresponse.info/api/v1/}), and Unhcr
+#'   ("unhcr", \url{https://kobocat.unhcr.org/api/v1/}) . For your own
+#'   installation, or other installations using the same API but accessed at a
+#'   different URL, enter the full URL.
 #' @param version Wether using the API "v1" or "v2"
 #'
 #' @author Ananda Mahto
@@ -23,12 +22,11 @@
 #' get_host_url("https://kobocat.unhcr.org/api/v1/")
 #'
 get_host_url <- function(api, version = "v1") {
-  if (api %in% c("kobo", "kobohr", "ona","unhcr")) {
+  if (api %in% c("kobo", "kobohr", "unhcr")) {
     if (version == "v1") {
       switch(api,
              kobo = "https://kc.kobotoolbox.org/api/v1",
              kobohr = "https://kc.humanitarianresponse.info/api/v1",
-             ona = "https://ona.io/api/v1",
              unhcr = "https://kobocat.unhcr.org/api/v1")
     } else if (version == "v2") {
       switch(api,
