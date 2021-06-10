c8d9ebe43244f456eea32f0102ea38d9fba685db
Fernando Cagua <f.cagua@cgiar.org>
2021-06-10 19:14:39 +1200

    Feat pds data (#40)
    * Adding functions to retrieve and ingest pds data and associated documentation
    
    * Updating pipeline including pds functions and associated github secrets
    
    * Updating with the news features of pds functions
    
    * Updated including the subtitles Survey and PDS under the title Data
    
    * Added pds related info including a new storage space linked to the pds bucket: pds_storage
    
    * updating pipeline for pds tracks ingestions: skipping trips preprocessing to ingest pds tracks
    
    * updating tracks ingestion with all trips ids
    
    * updating function description and remove some function' redundancies
    
    * updating function documentation
    
    * ingest pds tracks with few files
    
    * ingesting tracks directly within loop
    
    * adding file removal after ingestion
    
    * adding reauthentication into tracks ingestion function
    
    * adding reauthenticating before upload to refresh google token
    
    * Adding purrr::insistently to fix tracks uploading bug
    
    * fixing stale token bug
    
    * uploading tracks after loop
    
    * adding log info to trace tracks download and upload
    
    * Removing option for retrieving PDS trips as json as that's not possible
    
    * We actually not require more detail when listing files in the buckets
    
    * Use timor and timor-dev bucket for the trips
    
    * Make pds-track ingestion dependent on pds-trip ingestion
    
    * Get file identifier when it's downloaded from the bucket
    
    * Prefix for tracks to be "pds-track" as each file has only one track
    
    * Get PDS file IDs using existing functions (for consistency)
    
    * Retrieve tracks using `retrieve_pds_tracks_data()`. No merge necessary for now
    
    * Move query params outside of URL and request last-seen info too
    
    * Remove  `get_psd_resp()` as it's unused
    
    * FIx one typo from when data was merged
    
    * Return nothing when there are no objects in the bucket
    
    * Add option to compress files in the bucket
    
    * Run track ingestion in parallel using furrr so its faster
    
    * Run pipeline on macOS as it has three cores
    
    * Mac OS runners do not support docker in Github Actions
    
    * Fix miscellaneous package check problems
    
    * Updating function description
    
    Co-authored-by: langbart <lorenzo.longobardi@gmail.com>

---
+++ R/ingest-pds-data.R
@@ -0,0 +1,192 @@
+#' Ingest Pelagic Data System trips data
+#'
+#' Downloads Pelagic Data System (pds) trips information and uploads it to cloud
+#' storage services.
+#'
+#' This function downloads trips information from Pelagic Data System devices.
+#' Afterwards it uploads this information to cloud services. File names used
+#' contain a versioning string that includes the date-time and, if available,
+#' the first 7 digits of the git commit sha. This is acomplished
+#' using [add_version()]
+#'
+#' The parameters needed in `conf.yml` are:
+#'
+#' ```
+#' pds:
+#'   trips:
+#'     token:
+#'     secret:
+#'     file_prefix:
+#' pds_storage:
+#'   storage_name:
+#'     key:
+#'     options:
+#'       project:
+#'       bucket:
+#'       service_account_key:
+#' ```
+#'
+#' Progress through the function is tracked using the package *logger*.
+#'
+#'
+#' @param log_threshold The (standard Apache logj4) log level used as a
+#'   threshold for the logging infrastructure. See [logger::log_levels] for more
+#'   details
+#'
+#' @keywords workflow
+#'
+#' @return No output. This funcrion is used for it's side effects
+#' @export
+#'
+ingest_pds_trips <- function(log_threshold = logger::DEBUG){
+
+  logger::log_threshold(log_threshold)
+
+  pars <- read_config()
+
+  file_list <- retrieve_pds_trips(prefix = pars$pds$trips$file_prefix,
+                              secret = pars$pds$trips$secret,
+                              token = pars$pds$trips$token)
+
+  logger::log_info("Uploading files to cloud...")
+  # Iterate over multiple storage providers if there are more than one
+  purrr::map(pars$storage, ~ upload_cloud_file(file_list, .$key, .$options))
+
+  logger::log_success("File upload succeded")
+
+}
+
+#' Ingest Pelagic Data System tracks data
+#'
+#' Downloads Pelagic Data System (pds) trips information and uploads it to cloud
+#' storage services.
+#'
+#' The function  downloads and uploads only tracks data that are not yet
+#' stored in the bucket.
+#'
+#' The parameters needed in `conf.yml` are:
+#'
+#' ```
+#' pds:
+#'   tracks:
+#'     token:
+#'     secret:
+#'     file_prefix:
+#' pds_storage:
+#'   storage_name:
+#'     key:
+#'     options:
+#'       project:
+#'       bucket:
+#'       service_account_key:
+#' ```
+#'
+#' Progress through the function is tracked using the package *logger*.
+#'
+#'
+#' @param log_threshold The (standard Apache logj4) log level used as a
+#'   threshold for the logging infrastructure. See [logger::log_levels] for more
+#'   details
+#'
+#' @keywords workflow
+#'
+#' @return No output. This funcrion is used for it's side effects
+#' @export
+#' @importFrom rlang .data
+#'
+ingest_pds_tracks <- function(log_threshold = logger::DEBUG){
+
+  logger::log_threshold(log_threshold)
+
+  pars <- read_config()
+
+  pds_trips_csv <-
+    cloud_object_name(prefix = pars$pds$trips$file_prefix,
+                      provider = pars$storage$google$key,
+                      extension = "csv",
+                      options = pars$storage$google$options)
+  logger::log_info("Retrieving {pds_trips_csv}")
+  # get trips data frame
+  pds_trips_mat <- download_cloud_file(name = pds_trips_csv,
+                                       provider = pars$storage$google$key,
+                                       options = pars$storage$google$options)
+
+  # extract unique trip identifiers
+  trips_ID <- readr::read_csv(
+    pds_trips_mat,
+    col_types = readr::cols_only(Trip = readr::col_character())) %>%
+    magrittr::extract2("Trip") %>%
+    unique()
+
+  if (isTRUE(pars$pds$tracks$compress)) ext <- "csv.gz" else ext <- "csv"
+
+  # list id tracks already in bucket
+  file_list_id <- cloud_object_name(prefix = pars$pds$tracks$file_prefix,
+                    provider = pars$pds_storage$google$key,
+                    extension = ext,
+                    options = pars$pds_storage$google$options) %>%
+    stringr::str_extract("[[:digit:]]+") %>%
+    as.character()
+
+  process_track <- function(id, pars){
+
+    path <- paste0(pars$pds$tracks$file_prefix, "-", id) %>%
+      add_version(extension = "csv")
+    on.exit(file.remove(path))
+
+    retrieve_pds_tracks_data(path,
+                             secret = pars$pds$trips$secret,
+                             token = pars$pds$trips$token,
+                             id = id)
+
+    if (isTRUE(pars$pds$tracks$compress)) {
+      logger::log_info("Compressing file...")
+      csv_path <- path
+      path <- paste0(path, ".gz")
+      readr::read_csv(csv_path,
+                      col_types = readr::cols(.default = readr::col_character())) %>%
+        readr::write_csv(path)
+      on.exit(file.remove(csv_path, path))
+    }
+
+    logger::log_info("Uploading {path} to cloud...")
+    # Iterate over multiple storage providers if there are more than one
+    purrr::map(pars$pds_storage, ~ purrr::walk(
+      .x = path,
+      .f = ~ insistent_upload_cloud_file(
+        file = .,
+        provider = pars$pds_storage$google$key,
+        options = pars$pds_storage$google$options)))
+    logger::log_success("File upload succeded")
+
+  }
+
+  tracks_to_download <- trips_ID[!(trips_ID %in% file_list_id)]
+  if (isTRUE(pars$pds$tracks$multisession$parallel)) {
+    future::plan(future::multisession,
+                 workers = pars$pds$tracks$multisession$n_sessions)
+  }
+  furrr::future_walk(tracks_to_download, process_track, pars, .progress = TRUE)
+}
+
+#' Insistent version of `upload_cloud_file()`
+#'
+#' Just like `upload_cloud_file()`, this function takes a vector of tracks files
+#' as argument and upload them to the cloud. The function uses
+#' [purrr::insistently] in order to continue to upload files despite stale OAuth
+#' token.
+#'
+#' @param delay he time interval to suspend execution for, in seconds.
+#' @param ... Inputs to `upload_cloud_file()`
+#'
+#' @return No output. This function is used for it's side effects
+#' @export
+#'
+insistent_upload_cloud_file <- function(..., delay = 3){
+  purrr::insistently(upload_cloud_file,
+                     rate = purrr::rate_backoff(
+                       pause_cap = 60*5,
+                       max_times = 10),
+                     quiet = F)(...)
+  Sys.sleep(delay)
+}
---
+++ R/retrieve-pds-data.R
@@ -0,0 +1,145 @@
+
+#' Download pelagic data system trips in as csv or json
+#'
+#' @param path String with path to file where API request should be saved
+#' @param secret Access secret code for the account
+#' @param token Access token for the account
+#' @param start_date Start date from which download trips information
+#' @param end_date Last date to download trips information
+#' @param overwrite Will only overwrite existing path if TRUE.
+#'
+#' @return The file path
+#' @export
+#'
+#' @examples
+#' retrieve_pds_trips_data("test.csv")
+#' file.remove("test.csv")
+#'
+retrieve_pds_trips_data <- function(path, secret = NULL, token = NULL, start_date= NULL,
+                                end_date=NULL, overwrite = TRUE){
+
+  request_url <- paste("https://analytics.pelagicdata.com/api",token,
+                       "v1/trips",start_date, end_date, sep = "/")
+
+  httr::GET(url = request_url,
+            config = httr::add_headers('X-API-SECRET' = secret),
+            query = list(
+              deviceInfo = TRUE,
+              withLastSeen = TRUE
+            ),
+            httr::write_disk(path, overwrite = overwrite))
+  path
+}
+
+#' Download pelagic data system trips
+#'
+#' @param prefix Name to be used as the prefix of the file names to be
+#'   downloaded. Can be a path.
+#' @param secret
+#' @param token
+#' @param start_date
+#' @param end_date
+#' @param append_version whether to append versioning information to the
+#'   filename using [add_version].
+#' @inheritParams retrieve_pds_trips_data
+#'
+#' @return A character vector with paths of the downloaded files
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'   retrieve_pds_trips(prefix = "my-trips", secret = "abcXXXXXX",
+#'                   token = "123XXXXXX", start_date= "2018-07-01",
+#'                   end_date=Sys.Date())
+#'   # To download in a different path
+#'   dir.create("my-data-dir")
+#'   retrieve_pds_trips(prefix = "my-data-dir/my-trips", secret = "abcXXXXXX",
+#'                   token = "123XXXXXX", start_date= "2018-07-01",
+#'                   end_date=Sys.Date())
+#' }
+retrieve_pds_trips <- function(prefix, secret = NULL, token = NULL, start_date= "2018-07-01",
+                           end_date=Sys.Date(),
+                           append_version = TRUE){
+
+  data_filename <- paste(prefix, sep = "_")
+
+  if (isTRUE(append_version)) {
+    csv_filename <- add_version(data_filename, "csv")
+  } else {
+    csv_filename <- paste0(data_filename, ".csv")
+  }
+
+
+  logger::log_info("Downloading trips csv data as {csv_filename}...")
+  retrieve_pds_trips_data(csv_filename, secret, token,
+                          start_date, end_date)
+  logger::log_success("Trips csv data download succeeded")
+
+  csv_filename
+}
+
+
+#' Download pelagic data system tracks in as csv
+#'
+#' @param path String with path to file where API request should be saved
+#' @param secret Access secret code for the account
+#' @param token Access token for the account
+#' @param id Trip unique id from which download tracks data
+#' @param overwrite Will only overwrite existing path if TRUE.
+#'
+#' @return The file path
+#' @export
+#'
+#' @examples
+#' retrieve_pds_tracks_data("test.csv")
+#' file.remove("test.csv")
+retrieve_pds_tracks_data <- function(path, secret = NULL, token = NULL,
+                                 id = NULL, overwrite = TRUE){
+
+  request_url <- paste("https://analytics.pelagicdata.com/api", token,
+                       "v1/trips", id, "points", sep = "/")
+
+  httr::GET(url = request_url,
+            config = httr::add_headers('X-API-SECRET' = secret),
+            query = list(format = "csv"),
+            httr::write_disk(path, overwrite = overwrite))
+  path
+}
+
+
+#' Download pelagic data system tracks
+#'
+#' @param prefix Name to be used as the prefix of the file names to be
+#'   downloaded. Can be a path.
+#' @param secret
+#' @param token
+#' @param id
+#' @inheritParams retrieve_pds_tracks_data
+#'
+#' @return A character vector with paths of the downloaded files
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'   retrieve_pds_tracks(prefix = "my-tracks", secret = "abcXXXXXX",
+#'                   token = "123XXXXXX", id="2327722")
+#'   # To download in a different path
+#'   dir.create("my-data-dir")
+#'   retrieve_pds_tracks(prefix = "my-data-dir/my-tracks", secret = "abcXXXXXX",
+#'                   token = "123XXXXXX", id="2327722")
+#' }
+retrieve_pds_tracks <- function(prefix, secret, token,id){
+
+  data_filename <- paste(prefix,"raw",id,".csv",sep = "_")
+
+  filenames <- character()
+
+  logger::log_info("Downloading tracks csv data as {data_filename}...")
+  retrieve_pds_tracks_data(path = data_filename, secret, token, id)
+  logger::log_success("Tracks csv data download succeeded")
+  filenames <- c(filenames, data_filename)
+
+  filenames
+}
+
+
