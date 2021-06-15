64ace3e31552bed7dbf971250b050c009f70f5c9
langbart <lorenzo.longobardi@gmail.com>
2021-06-15 20:28:43 +0200

    Adding pds duration and distance validation function and updating documentation

--- R/validation-functions.R
+++ R/validation-functions.R
@@ -49,13 +49,13 @@ validate_this_imei <- function(this_imei, this_id = NULL, valid_imeis){
 }
 
 
-#' Validate surveys' temporal info
+#' Validate surveys' temporal parameters
 #'
 #' This function takes a preprocessed landings' matrix and validate temporal
 #' info associated to each survey.
 #'
 #' @param data A preprocessed data frame
-#' @param hrs Limit of trip duration in hours to be considered a standard
+#' @param hrs Limit of trip duration in hours to be considered a valid
 #' catch session.
 #'
 #' @return A list containing data frames with validated catch dates and
@@ -132,13 +132,35 @@ validate_catch_value <- function(data,method="MAD",k=13){
 }
 
 
-# Validate and replace catches length and n° individuals
-validate_length <- function(data,method="MAD",k=13){
+#' Validate surveys' catch parameters
+#'
+#' This function takes a preprocessed landings' matrix and uses univariate
+#' techniques (see [univOutl::LocScaleB]) for the identification of outliers in
+#' the distribution of the number of individuals per catch and their size. The
+#' function returns a data frame with the survey id, the alert number and
+#' a nested column `species_group` containing validated catches parameters. By
+#' default the function uses the method of the median absolute deviation (MAD)
+#' for outliers identification.
+#'
+#' @param data A preprocessed data frame
+#' @inheritParams univOutl::LocScaleB
+#'
+#' @return A data frame containing the validated catches parameters.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'   pars <- read_config()
+#'   landings <- get_merged_landings(pars)
+#'   validate_catch_params(landings,method="MAD",k=13)
+#' }
+#'
+validate_catch_params <- function(data,method="MAD",k=13){
 
   catches_dat_unnested <-  landings %>%
     dplyr::select(`_id`,species_group) %>%
-    tidyr::unnest(.,species_group) %>%
-    tidyr::unnest(.,length_individuals) %>%
+    tidyr::unnest(.,species_group,keep_empty = TRUE) %>%
+    tidyr::unnest(.,length_individuals,keep_empty = TRUE) %>%
     dplyr::select(`_id`,n,species,mean_length,n_individuals) %>%
     dplyr::mutate(dplyr::across(c(mean_length,n_individuals),.fns = as.numeric))
 
@@ -146,7 +168,7 @@ validate_length <- function(data,method="MAD",k=13){
   individuals_dat <- NULL
   for(i in unique(catches_dat_unnested$species)){
 
-    if(i %in% c("0","300")){next}
+    if(i %in% c("0","300",NA)){next}
     else{
       spe_code <- i
       limits_length <-
@@ -156,7 +178,7 @@ validate_length <- function(data,method="MAD",k=13){
         magrittr::extract2(2) %>%t() %>%
         dplyr::as_tibble() %>%
         dplyr::rename(length_low=lower.low,length_up=upper.up) %>%
-        dplyr::mutate(species=i,length_low=case_when(length_low<=0~0,TRUE~length_low))
+        dplyr::mutate(species=i,length_low=dplyr::case_when(length_low<=0~0,TRUE~length_low))
 
       limits_individuals <-
         dplyr::filter(catches_dat_unnested,species %in% i & n_individuals>0)  %>%
@@ -165,7 +187,7 @@ validate_length <- function(data,method="MAD",k=13){
         magrittr::extract2(2) %>% t() %>%
         dplyr::as_tibble() %>%
         dplyr::rename(inds_low=lower.low,inds_up=upper.up) %>%
-        dplyr:: mutate(species=i,inds_low=case_when(inds_low<=0~0,TRUE~inds_low))
+        dplyr:: mutate(species=i,inds_low=dplyr::case_when(inds_low<=0~0,TRUE~inds_low))
 
     }
     length_dat <- rbind(length_dat,limits_length)
@@ -177,24 +199,96 @@ validate_length <- function(data,method="MAD",k=13){
 
   validated_length <-
     dplyr::left_join(catches_dat_unnested,limits) %>%
-    dplyr::transmute(n_individuals=
-                       dplyr::case_when(.$n_individuals < 0 ~ .$n_individuals * -1,
-                                        .$n_individuals > 0 & .$n_individuals < .$inds_low ~ NA_real_,
-                                        .$n_individuals > .$inds_up ~ NA_real_,
-                                        .$n_individuals >0 & .$mean_length < .$length_low |
-                                          .$n_individuals >0 & .$mean_length > .$length_up ~ NA_real_,
-                                        TRUE ~ .$n_individuals),
-                     alert_number=
-                       dplyr::case_when(.$n_individuals > 0 & .$n_individuals < .$inds_low |
-                                          .$n_individuals > .$inds_up |
-                                          .$n_individuals >0 & .$mean_length < .$length_low |
-                                          .$n_individuals >0 & .$mean_length > .$length_up ~ 7,
-                                        TRUE ~ NA_real_),
-                     mean_length=.$mean_length,
-                     submission_id=as.integer(.$`_id`),
-                     n=.$n,
-                     species=.$species) %>%
+    dplyr::mutate(alert_number=
+                    dplyr::case_when(abs(.$n_individuals) < .$inds_low |
+                                     .$n_individuals > .$inds_up |
+                                     .$n_individuals >0 & .$mean_length < .$length_low |
+                                     .$n_individuals >0 & .$mean_length > .$length_up ~ 7,
+                                     TRUE ~ NA_real_),
+                  n_individuals=
+                    dplyr::case_when(.$n_individuals < 0 ~ .$n_individuals * -1,
+                                     .$n_individuals > .$inds_up  |
+                                     .$n_individuals >0 & .$mean_length < .$length_low |
+                                     .$n_individuals >0 & .$mean_length > .$length_up ~ NA_real_,
+                                     TRUE ~ .$n_individuals),
+                  submission_id=.$`_id`) %>%
     dplyr::select(submission_id,n,species,mean_length,n_individuals,alert_number)
 
-  validated_length
+
+  # extract alert number
+  alert_number <- validated_length %>%
+    dplyr::select(submission_id,n,alert_number) %>%
+    dplyr::group_by(submission_id) %>%
+    dplyr::filter(dplyr::row_number() ==1)
+
+  # nest validated data
+  validated_length_nested <-# sembra ok
+    validated_length %>%
+    dplyr::select(-alert_number) %>%
+    dplyr::group_by(submission_id,n,species) %>%
+    tidyr::nest(length_individuals = c(mean_length,n_individuals))
+
+  # replace with validated catches params
+  validated_catch_params <- landings %>%
+    dplyr::rename(submission_id=`_id`) %>%
+    #dplyr::mutate(submission_id=as.integer(submission_id)) %>%
+    dplyr::select(submission_id,species_group) %>%
+    tidyr::unnest(species_group,keep_empty = TRUE) %>%
+    dplyr::mutate(length_individuals=validated_length_nested$length_individuals)%>%
+    dplyr::group_by(submission_id) %>%
+    tidyr::nest() %>%
+    dplyr::rename("species_group" = "data") %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(alert_number=alert_number$alert_number)
+
+  validated_catch_params
+}
+
+
+
+#' Validate pds trips duration and distance
+#'
+#' This function takes pds trips information to return validated data frames of
+#' trip duration and distance.
+#'
+#' @param data A  data frame
+#' @param hrs Limit of trip duration in hours to be considered a valid
+#' catch session.
+#' @param km Limit of trip distance traveled in Km to be considered a valid
+#' catch session.
+#'
+#' @return A list containing data frames with validated catch duration and
+#' catch distance traveled
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'   pars <- read_config()
+#'   pds_trips <- retrieve_pds_trips(prefix = pars$pds$trips$file_prefix,
+#'                                   secret = pars$pds$trips$secret,
+#'                                   token = pars$pds$trips$token)%>%
+#'  readr::read_csv(col_types = readr::cols(.default = readr::col_character()))
+#'
+#'   validate_pds_navigation(pds_trips)
+#' }
+validate_pds_navigation <- function(data, hrs =18, km=60){
+
+  validated_duration <- list(
+
+    validated_pds_duration = pds_trips %>%
+      dplyr::mutate(`Duration (Seconds)` = as.numeric(`Duration (Seconds)`)) %>%
+      dplyr::transmute(alert_number=
+                         dplyr::case_when(.$`Duration (Seconds)`> hrs*60^2 ~8 ,TRUE ~ NA_real_),
+                       `Duration (Seconds)`=
+                         dplyr::case_when(.$`Duration (Seconds)` > hrs*60^2 ~ NA_real_,TRUE ~ .$`Duration (Seconds)`),
+                       Trip=.$Trip),
+
+    validated_pds_distance = pds_trips %>%
+      dplyr::mutate(`Distance (Meters)` = as.numeric(`Distance (Meters)`)) %>%
+      dplyr::transmute(alert_number=
+                         dplyr::case_when(.$`Distance (Meters)`> km*1000 ~ 9 ,TRUE ~ NA_real_),
+                       `Distance (Meters)`=
+                         dplyr::case_when(.$`Distance (Meters)` > km*1000 ~ NA_real_,TRUE ~ .$`Distance (Meters)`),
+                       Trip=.$Trip))
+  validated_duration
 }
