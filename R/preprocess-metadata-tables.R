b6a62e9d7fa3b75ca2c63803852feb61e831b09c
langbart <lorenzo.longobardi@gmail.com>
2021-06-29 13:54:36 +0200

    adding fao_catch table to metadata preprocessing

--- R/preprocess-metadata-tables.R
+++ R/preprocess-metadata-tables.R
@@ -59,7 +59,8 @@ preprocess_metadata_tables <- function(log_threshold = logger::DEBUG){
     devices = pt_validate_devices(metadata_tables$devices),
     device_installs = pt_validate_vms_installs(metadata_tables$vms_installs),
     boats = pt_validate_boats(metadata_tables$boats),
-    catch_types = pt_validate_catch_types(metadata_tables$catch_types))
+    catch_types = pt_validate_catch_types(metadata_tables$catch_types),
+    fao_catch = pt_validate_fao_catch(metadata_tables$fao_catch))
 
   preprocessed_filename <- paste(pars$metadata$airtable$name,
                                  "preprocessed", sep = "_") %>%
@@ -199,3 +200,16 @@ pt_validate_boats <- function(boats_table){
 pt_validate_catch_types <- function(catch_type_table){
   catch_type_table
 }
+
+#' Parse and validate fao catch table
+#'
+#' Currently this function does not perform anything and is a placeholder
+#'
+#' @param fao_catch_table a data frame with the catch type info
+#'
+#' @return a tibble
+#'
+pt_validate_fao_catch <- function(fao_catch_table){
+  fao_catch_table
+}
+
