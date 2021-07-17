ecfb1d75dc5ea912b91264626616fbaff5ada1d6
Fernando Cagua <efcaguab@gmail.com>
2021-07-17 18:47:56 +1200

    Add comment explaining why the wd is changed

--- inst/tinytest/test_validated_landings.R
+++ inst/tinytest/test_validated_landings.R
@@ -1,5 +1,7 @@
 
 logger::log_threshold(logger::ERROR)
+# Adjust the working directory so that when running localy the authentication
+# details in the config file load properly
 setwd("../..")
 pars <- peskas.timor.data.pipeline::read_config()
 
