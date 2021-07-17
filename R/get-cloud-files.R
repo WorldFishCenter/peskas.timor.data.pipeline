d5681d447c2aca6390b15c378b901bf9caf43f32
Fernando Cagua <efcaguab@gmail.com>
2021-07-17 20:17:46 +1200

    Aux function to get merged trips

--- R/get-cloud-files.R
+++ R/get-cloud-files.R
@@ -20,3 +20,15 @@ get_validated_pds_trips <- function(pars){
       options = pars$storage$google$options) %>%
     readr::read_rds()
 }
+
+
+get_merged_trips <- function(pars){
+  cloud_object_name(
+    prefix = paste0(pars$merged_trips$file_prefix),
+    provider = pars$storage$google$key,
+    options = pars$storage$google$options) %>%
+    download_cloud_file(
+      provider = pars$storage$google$key,
+      options = pars$storage$google$options) %>%
+    readr::read_rds()
+}
