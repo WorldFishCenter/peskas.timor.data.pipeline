c1396d6d908eeae12e0d4a3bd9c1363d124f40cd
Fernando Cagua <efcaguab@gmail.com>
2021-07-26 12:44:46 +1200

    Add tests for the public data

---
+++ inst/tinytest/test_public_data.R
@@ -0,0 +1,23 @@
+library(peskas.timor.data.pipeline)
+
+logger::log_threshold(logger::ERROR)
+# Adjust the working directory so that when running localy the authentication
+# details in the config file load properly
+setwd("../..")
+pars <- peskas.timor.data.pipeline::read_config()
+
+public_files <- get_public_files(pars)
+
+expect_false(
+  any(duplicated(public_files$trips$trip_id)),
+  info = "anonimised trip ids are duplicated"
+)
+
+expect_true(
+  public_files$aggregated %>%
+    purrr::map_int(~ dplyr::filter(., is.na(.$date_bin_start))$n_matched) %>%
+    magrittr::equals(0) %>%
+    any(),
+  info = "A trip has been matched in an NA date"
+)
+
