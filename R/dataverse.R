947034c2694e030e7c7f16c8eeb0c7fc35cccd13
langbart <lorenzo.longobardi@gmail.com>
2021-08-02 17:07:34 +0200

    Adding documentation to data exporting functions

--- R/dataverse.R
+++ R/dataverse.R
@@ -1,71 +1,14 @@
-#' Export file to Dataverse
+#' Publish a Dataverse repository
 #'
-#' This function exports a file to a specified Dataverse repository.
+#' This function publish a specific Dataverse repository.
 #'
-#' @param token API token associated to Dataverse account.
-#' @param dataverse A character string specifying a Dataverse name.
-#' @param upfile File to be exported.
-#' @param metadat A list of metadata in Dublin Core format, see
-#' \url{https://www.dublincore.org/specifications/dublin-core/dcmi-terms/}.
+#' @param token API token associated to the Dataverse account.
+#' @param dataverse A character string specifying the Dataverse ID.
 #' @param server A character string specifying a Dataverse server.
 #'
 #' @export
 #'
 #' @examples
-#' \dontrun{
-#' metadat <- list(
-#'   title = paste("My dataset", format(Sys.time(), "%Y%m%d_%H%M")),
-#'   creator = "A big dinosaur",
-#'   description = "This is an amazing dataset...",
-#'   language = "English"
-#' )
-#'
-#' upl_files <- "/Users/..../my_data.csv"
-#'
-#' export_dataverse(
-#'   token = "a3k2XXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX",
-#'   dataverse = "my_dataverse",
-#'   upfile = file,
-#'   metadat = metadat
-#' )
-#' }
-#'
-export_dataverse <- function(token, dataverse, upfile, metadat, server) {
-  ds <- dataverse::initiate_sword_dataset(
-    dataverse = dataverse,
-    body = metadat,
-    key = token,
-    server = server
-  )
-
-  contents <- dataverse::dataverse_contents(dataverse = dataverse, server = server, key = token)
-  last_content <- contents[[length(contents)]]
-  mydatadoi <- paste0(
-    last_content$protocol, ":",
-    last_content$authority, "/",
-    last_content$identifier
-  )
-
-  params <- list(
-    `persistentId` = mydatadoi,
-    `release` = "yes"
-  )
-
-  files <- list(
-    `file` = httr::upload_file(upfile),
-    `jsonData` = jsonlite::toJSON(metadat, auto_unbox = TRUE)
-  )
-
-  httr::POST(
-    url = paste0("https://", server, "/api/datasets/:persistentId/add"),
-    httr::add_headers(`X-Dataverse-key` = token),
-    query = params,
-    body = files
-  )
-}
-
-
-# publish a dataverse
 publish_dataverse <- function(token, dataverse, server) {
   url <- paste0("https://", server, "/api/dataverses/", dataverse, "/actions/:publish")
   res <- httr::POST(
@@ -75,7 +18,19 @@ publish_dataverse <- function(token, dataverse, server) {
   res
 }
 
-# get metadata information from landings
+#' Retrieve metadata information from survey landings
+#'
+#' This function downloads the latest version of landings and extract metadata
+#' information.
+#'
+#' @param pars Configuration file
+#'
+#' @return A list with metadata information:
+#' extent: time period covered
+#' ....
+#' @export
+#'
+#' @examples
 get_metadata_info <- function(pars) {
   landings <- get_merged_landings(pars)
   extent <- paste(min(landings$date), max(landings$date), sep = "-")
@@ -84,16 +39,24 @@ get_metadata_info <- function(pars) {
   landings_metadata
 }
 
-# generate a list of metadata information to append to data upload
+#' Generate a list of metadata
+#'
+#' The function generate a list of metadata information to append to the
+#' files to upload to a Dataverse repository.
+#'
+#' @return A list with metadata information
+#' @export
+#'
+#' @examples
 generate_metadata <- function() {
   pars <- read_config()
   landings_metadata <- get_metadata_info(pars)
 
   metadat <- list(
-    title = paste("Project test"),
-    creator = "The cat",
+    title = paste("Project title"),
+    creator = "Creator of the project",
     created = as.character(Sys.Date()),
-    description = "This is an incredible dataset, full of incredible stuff and numbers",
+    description = "Description of the data",
     extent = as.character(landings_metadata$extent),
     language = "English"
   )
@@ -101,8 +64,19 @@ generate_metadata <- function() {
   metadat
 }
 
-# add files to the latest dataset created
-upload_files <- function(file_list, token, dataverse, server) {
+#' Upload files to Dataverse
+#'
+#' This function upload a list files in a specific Dataverse repository.
+#'
+#' @param file_list Paths indicating the files to be uploaded to Dataverse.
+#' @param token API token associated to the Dataverse account.
+#' @param dataverse A character string specifying the Dataverse ID.
+#' @param server A character string specifying a Dataverse server.
+#'
+#' @export
+#'
+#' @examples
+upload_files <- function(file_list = NULL, token = NULL, dataverse = NULL, server = NULL) {
   dataverse_content <-
     dataverse::dataverse_contents(
       dataverse = dataverse,
@@ -129,8 +103,17 @@ upload_files <- function(file_list, token, dataverse, server) {
 }
 
 
-# publish the last dataset of a specific dataverse repository
-release_last_dataset <- function(token, dataverse, server) {
+#' Publish latest dataset created
+#'
+#' The function converts from "draft" to "public" the latest dataset uploaded
+#' in a specific Dataverse repository.
+#'
+#' @inheritParams upload_files
+#'
+#' @export
+#'
+#' @examples
+publish_last_dataset <- function(token = NULL, dataverse = NULL, server = NULL) {
   dataverse_content <-
     dataverse::dataverse_contents(
       dataverse = dataverse,
@@ -148,10 +131,17 @@ release_last_dataset <- function(token, dataverse, server) {
   )
 }
 
-# upload and publish data in dataverse
-export_files <- function(token, dataverse, file_list, metadat, server) {
-  pars <- read_config()
-
+#' Upload and publish a dataset on Dataverse
+#'
+#' This function upload and publish data on a specific Dataverse repository
+#' including associated metadata information.
+#'
+#' @inheritParams upload_files
+#'
+#' @export
+#'
+#' @examples
+export_files <- function(token, dataverse, server) {
   metadat <- generate_metadata()
 
   dataverse::initiate_sword_dataset(
@@ -161,15 +151,18 @@ export_files <- function(token, dataverse, file_list, metadat, server) {
     server = server
   )
 
-  file_list <- # get files from cloud
+  file_list <- # complete with files to export
 
     upload_files(
-      file_list,
-      token,
-      dataverse,
-      server
+      file_list = file_list,
+      token = token,
+      dataverse = dataverse,
+      server = server
     )
 
-
-  release_last_dataset(token, dataverse, server)
+  publish_last_dataset(
+    token = token,
+    dataverse = dataverse,
+    server = server
+  )
 }
