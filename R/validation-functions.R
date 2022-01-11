fe776dcb522edd9ffe163e6c383dc9a82644ba23
langbart <lorenzo.longobardi@gmail.com>
2022-01-11 14:08:09 +0100

    fix .data notes

--- R/validation-functions.R
+++ R/validation-functions.R
@@ -432,7 +432,7 @@ validate_n_fishers <- function(landings, method, k){
                   fisher_number_woman = .data$`trip_group/no_fishers/no_women_fishers`) %>%
     dplyr::mutate(dplyr::across(tidyselect::starts_with("fisher"), as.numeric)) %>%
     dplyr::mutate(dplyr::across(tidyselect::starts_with("fisher"), list(alert = alert_outlier), alert_if_larger = 18, alert_if_smaller = 18, k = k, logt = T, method = method)) %>%
-    dplyr::mutate(alert_number = dplyr::coalesce(fisher_number_child, fisher_number_man, fisher_number_woman)) %>%
+    dplyr::mutate(alert_number = dplyr::coalesce(.data$fisher_number_child, .data$fisher_number_man, .data$fisher_number_woman)) %>%
     dplyr::mutate(dplyr::across(tidyselect::starts_with("fisher"), ~dplyr::if_else(is.na(alert_number), NA_real_, .))) %>%
     dplyr::select(-tidyselect::ends_with("alert")) %>%
     # Fixing types
