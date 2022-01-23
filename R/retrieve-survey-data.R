ee4ed2d90a09f60b4e135b0fa9ca7c2e220440a1
langbart <lorenzo.longobardi@gmail.com>
2022-01-23 19:11:43 +0100

    Update ONA api path

--- R/retrieve-survey-data.R
+++ R/retrieve-survey-data.R
@@ -6,7 +6,7 @@
 #'   are made available for KoBo Toolbox ("kobo",
 #'   \url{https://kc.kobotoolbox.org/api/v1/}), KoBo Humanitarian Response
 #'   ("kobohr", \url{https://kc.humanitarianresponse.info/api/v1/}), Ona ("ona",
-#'   \url{https://ona.io/api/v1/}) and Unhcr ("unhcr",
+#'   \url{https://api.ona.io/api/v1/}) and Unhcr ("unhcr",
 #'   \url{https://kobocat.unhcr.org/api/v1/}) . For your own installation, or
 #'   other installations using the same API but accessed at a different URL,
 #'   enter the full URL.
@@ -28,7 +28,7 @@ get_host_url <- function(api, version = "v1") {
       switch(api,
              kobo = "https://kc.kobotoolbox.org/api/v1",
              kobohr = "https://kc.humanitarianresponse.info/api/v1",
-             ona = "https://ona.io/api/v1",
+             ona = "https://api.ona.io/api/v1",
              unhcr = "https://kobocat.unhcr.org/api/v1")
     } else if (version == "v2") {
       switch(api,
