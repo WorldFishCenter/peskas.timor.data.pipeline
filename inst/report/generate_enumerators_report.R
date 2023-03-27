eb63b17cbf6d25bf43d6aff8fc6a926b9b92904c
langbart <lorenzo.longobardi@gmail.com>
2023-03-27 18:03:25 +0200

    Generate enumerators report on a daily basis

--- inst/report/generate_enumerators_report.R
+++ inst/report/generate_enumerators_report.R
@@ -1,15 +1,22 @@
 library(magrittr)
 
-month <- seq(lubridate::floor_date(Sys.Date(), "month"), length = 2, by = "-1 months")[2]
-month <- lubridate::month(month, label = T)
-year <- lubridate::year(Sys.Date())
+#month <- seq(lubridate::floor_date(Sys.Date(), "month"), length = 2, by = "-1 months")[2]
+#month <- lubridate::month(month, label = T)
+#year <- lubridate::year(Sys.Date())
 
-filename <- paste(month, year, "enumerators_summary_report", sep = "_")
-filename <- paste0(filename, ".pdf")
+#filename <- paste(month, year, "enumerators_summary_report", sep = "_")
+filename <- paste0("enumerators_summary_report", ".html")
 
 rmarkdown::render(
   input = system.file("report/enumerators_summary.Rmd", package = "peskas.timor.data.pipeline"),
   output_dir = system.file("report", package = "peskas.timor.data.pipeline"),
   clean = FALSE,
   output_file = filename
+)
+
+peskas.timor.data.pipeline::upload_cloud_file(
+  file = system.file(path, package = "peskas.timor.data.pipeline"),
+  name = filename,
+  provider = pars$public_storage$google$key,
+  options = pars$public_storage$google$options
 )
