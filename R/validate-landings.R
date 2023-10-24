0ebf5765871d3fef1002ef49acf66126ec2eebe7
langbart <lorenzo.longobardi@gmail.com>
2023-10-24 11:13:21 +0200

    Style code

--- R/validate-landings.R
+++ R/validate-landings.R
@@ -68,9 +68,11 @@ validate_landings <- function(log_threshold = logger::DEBUG) {
     k_ind = pars$validation$landings$catch$n_individuals$k
   )
   logger::log_info("Generating catches parameters bounds table...")
-  bounds_table <- get_bounds_table(data = regular_landings_data,
-                                   metadata_table = metadata,
-                                   k_ind = pars$validation$landings$catch$n_individuals$k)
+  bounds_table <- get_bounds_table(
+    data = regular_landings_data,
+    metadata_table = metadata,
+    k_ind = pars$validation$landings$catch$n_individuals$k
+  )
   price_weight_alerts <- validate_price_weight(
     catch_alerts = surveys_catch_alerts,
     price_alerts = surveys_price_alerts,
