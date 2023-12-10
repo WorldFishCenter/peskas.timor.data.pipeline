0bbbba9698c5dc409211cc56dc691f29ccbc4968
langbart <lorenzo.longobardi@gmail.com>
2023-12-10 17:11:20 +0100

    fix nutrients and catch order

--- R/format-public-data.R
+++ R/format-public-data.R
@@ -832,7 +832,7 @@ jsonify_nutrients <- function(data) {
     dplyr::ungroup()
 
   df_split <- df_ord %>% split(.$nutrient)
-  df_split_ord <- df_split[c("Protein", "Zinc", "Omega-3", "Calcium", "Vitamin A", "Iron")]
+  df_split_ord <- df_split[c("Protein", "Zinc", "Vitamin A", "Calcium", "Omega-3", "Iron")]
 
   dat <- lapply(names(df_split_ord), function(nutrient) {
     org_data <- df_split_ord[[nutrient]]
